6e003916878e39beebbef492d295cc5ad7d80ae5
pavel-kostyanov <kspavel777@gmail.com>
2017-11-16 23:57:44 +0300

    second lesson of minesweeper app

---
+++ README.md
@@ -0,0 +1,4 @@
+Codecademy course "Build Front-End Web Applications from Scratch"
+Nov14 2017.  
+Creation of the training application - Minesweeper.
+Author Pavel Kostyanov.
--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,5 +1,28 @@
-const blankLine = '  |   |  ';
-console.log('This is what an empty board would look like:');
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
+// const blankLine = '  |   |  ';
+// const guesLine = '1 |   |  ';
+// const bombLine = '  | B |  ';
+// console.log('This is what an empty board would look like:');
+// console.log(blankLine);
+// console.log(blankLine);
+// console.log(blankLine);
+// console.log('This is what a board with a guess and a bomb on it would look like:');
+// console.log(guesLine);
+// console.log(bombLine);
+// console.log(blankLine);
+const printBoard = (board) => {
+  console.log('Current Board:');
+  console.log(board[0].join(' | '));
+  console.log(board[1].join(' | '));
+  console.log(board[2].join(' | '));
+};
+let board = [
+             [' ', ' ', ' '],
+             [' ', ' ', ' '],
+             [' ', ' ', ' ']
+            ];
+printBoard(board);
+
+board[0][1] = '1';
+board[2][2] = 'B';
+
+printBoard(board);
